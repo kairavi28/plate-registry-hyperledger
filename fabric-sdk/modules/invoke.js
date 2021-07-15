ba685e7ccc7c81a744cc34b3ebc034e9b48f645c
nimaghazanfari <ghazanfari_nima@yahoo.com>
2021-07-15 15:00:57 +0000

    appUser is the name for application user

--- fabric-sdk/modules/invoke.js
+++ fabric-sdk/modules/invoke.js
@@ -16,12 +16,7 @@ async function main() {
         const ccpPath = path.resolve(__dirname, '..', '..', '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
         let ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
 
-        const [appUser, contractName, functionName] = process.argv.slice(2);
-
-        if (!appUser) {
-            console.error('No valid user!');
-            return;
-        }
+        const [contractName, functionName] = process.argv.slice(2);
 
         if (!contractName) {
             console.error('Pass the contract name.');
@@ -39,7 +34,7 @@ async function main() {
         console.log(`Wallet path: ${walletPath}`);
 
         // Check to see if we've already enrolled the user.
-        const identity = await wallet.get(appUser);
+        const identity = await wallet.get('appUser');
         if (!identity) {
             console.log('An identity for the user "appUser" does not exist in the wallet');
             console.log('Run the registerUser.js application before retrying');
@@ -48,7 +43,7 @@ async function main() {
 
         // Create a new gateway for connecting to our peer node.
         const gateway = new Gateway();
-        await gateway.connect(ccp, { wallet, identity: appUser, discovery: { enabled: true, asLocalhost: true } });
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
 
         // Get the network (channel) our contract is deployed to.
         const network = await gateway.getNetwork('mychannel');
