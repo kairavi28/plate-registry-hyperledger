6d2e46a73bfb8a7bbea82851b08ce17dcb8a34f2
nimaghazanfari <ghazanfari_nima@yahoo.com>
2021-07-16 09:14:10 +0000

    index updated

--- chaincode/index.js
+++ chaincode/index.js
@@ -9,9 +9,13 @@
 const Owner = require('./lib/owner');
 const Plate = require('./lib/plate');
 const Payment = require('./lib/payment');
+const Vehicle = require('./lib/vehicle');
+const Assigned_Plate = require('./lib/assigned_plate');
 
 module.exports.Owner = Owner;
 module.exports.Plate = Plate;
 module.exports.Payment = Payment;
+module.exports.Vehicle = Vehicle;
+module.exports.Assigned_Plate = Assigned_Plate;
 
-module.exports.contracts = [Owner, Plate, Payment];
+module.exports.contracts = [Owner, Plate, Payment, Vehicle, Assigned_Plate];
